e8c2df366e13353b8041eba179d9db34d96c7ec8
haibienne <haibien165@gmail.com>
2023-12-04 22:59:30 +0700

    init

---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,10 @@
+<center><h3>• Website đồ án tốt nghiệp được chạy trên nền tảng host ảo localhost	<br/>
+  "Công ty CP Đầu tư Organica - Phát triển và phân phối thực phẩm hữu cơ cho người Việt" được thiết kế theo tiêu chi dễ 	<br/>
+ nhìn thân thiện với người dùng
+	<br/>
+• Được thực hiện bởi sinh viên:
+	<br/>
+○ Trần Văn hải
+	<br/>
+	<br/>
+</h3></center>
